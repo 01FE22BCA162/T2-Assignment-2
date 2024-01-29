24d9df1e9616be0bf6b736517fb4150eb05240be
Sanjana A M <01fe22bca162@kletech.ac.in>
2024-01-29 16:19:24 +0530

    Upadated code changes to courses with their marks.

---
+++ StudentCourses.java
@@ -0,0 +1,14 @@
+public class StudentCourses {
+
+    private String[] courseCodes = { "C#.Net   /C Sharp","Software Testing", "Machine Learning", "DEVOPS       LAB", "Logical Reasoning" };
+    private int[] marks = { 92, 98, 95, 91, 97 };
+
+    public void displayStudentMarks() {
+        System.out.println("Course names      \t\tMarks-Obtained");
+
+        for (int i = 0; i < courseCodes.length; i++) {
+                System.out.println(courseCodes[i] + "\t\t\t" + marks[i]);
+        }
+    }
+
+}
